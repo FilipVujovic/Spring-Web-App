96db444f0ea947b3ad250a3bf6422bd8596dd37f
FilipVujovic <44211198+FilipVujovic@users.noreply.github.com>
2020-06-01 20:15:56 +0200

    Initial Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,41 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 import { AppComponent } from './app.component';
-
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { HomeComponent } from './components/core/home/home.component';
+import { AuthorComponent } from './components/core/author/author.component';
+import { AboutComponent } from './components/core/about/about.component';
+import { NacionalnostComponent } from './components/nacionalnost/nacionalnost.component';
+import { LigaComponent } from './components/liga/liga.component';
+import { TimComponent } from './components/tim/tim.component';
+import { IgracComponent } from './components/igrac/igrac.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AuthorComponent,
+    AboutComponent,
+    NacionalnostComponent,
+    LigaComponent,
+    TimComponent,
+    IgracComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatIconModule,
+    MatSidenavModule,
+    MatListModule,
+    MatGridListModule,
+    MatExpansionModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
